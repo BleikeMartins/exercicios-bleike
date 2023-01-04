15e1a00f8ed123854e25160a02c5a70ca4dac771
BleikeMartins <luiznandinho98@gmail.com>
2023-01-04 18:34:28 -0300

    Exercicio 2

---
+++ exercicios-bleike/01 operadoresAritimeticas.js
@@ -0,0 +1,7 @@
+let num1 = 4;
+let num2 = 4;
+
+console.log('Soma: 2+4 =' + (num1 + num2));
+console.log('Subtracao: 2-4 =' + (num1 - num2));
+console.log('Divisao: 2/4 =' + (num1 / num2));
+console.log('Multiplicacao: 2*4 =' + (num1 * num2));
---
+++ exercicios-bleike/02 ValorJantar.js
@@ -0,0 +1,24 @@
+//EXERCICIO 2 
+
+// Elaborar um programa que leia o valor de um jantar. Calcule e informe o valor da taxa do garçom (10%) e o 
+//valor total a ser pago.
+
+//Exemplo:
+// Valor do jantar: R$80,00
+// taxa do garçom: R$8,00
+// total a pagar: R$88,00
+
+let valorJantar = 80;
+let TaxaGarcom = 10;
+let total = 0;
+
+total = 80 + ((TaxaGarcom /100) * 80);
+
+  console.log((TaxaGarcom/100) * 80);
+
+  console.log(`valor do jantar R$ ${valorJantar},00`)
+  console.log(`Taxa Do Garçom R$ ${(TaxaGarcom / 100) * 80},00`)
+  console.log(`Total R$ ${total},00`)
+
+
+
